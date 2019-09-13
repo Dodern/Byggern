d574ede6ab4b9171269bb3aa12dd30ee065aae49
Vebjørn Johansen Rognli <vebjornjr@gmail.com>
2019-09-13 14:25:02 +0200

    functional SRAM with GAL IC

--- src/xmem.c
+++ src/xmem.c
@@ -1,17 +1,17 @@
 #include "xmem.h"
 
-
 void xmem_init(void){
     MCUCR |= (1 << SRE);//enable XMEM
     SFIOR |= (1 << XMM2);//mask unused bits PC7-PC4
 }
 
-// void xmem_write(uint8_t data, uint16_t addr){
-//     IO_MEMORY->SRAM[addr]=data;
-// }
+void xmem_write(uint8_t data, uint16_t addr, uint16_t base_address){
+    volatile char *ext_mem = (char *) base_address; // Start address for the SRAM
+    ext_mem[addr] = data;
+}
 
-// uint8_t xmem_read(uint16_t addr){
-//     volatile char *ext_mem= (char *) BASE_ADDRESS;
-//     uint8_t ret_val=ext_mem[addr];
-//     return ret_val;
-// }
+uint8_t xmem_read(uint16_t addr, uint16_t base_address){
+    volatile char *ext_mem = (char *) base_address; // Start address for the SRAM
+    uint8_t ret_val = ext_mem[addr];
+    return ret_val;
+}
--- src/xmem.h
+++ src/xmem.h
@@ -1,19 +1,16 @@
-#pragma once
+#ifndef XMEM_H
+#define XMEM_H
+
 #include <avr/io.h>
 
-#define BASE_ADDRESS 0x1000
-
 // Address Defintions
-// #define IO_MEMORY ((AVR162_IO_MEMORY_REGS*)0x1000)
-
-// // Address Structs
-// typedef struct {
-//     volatile uint16_t OLED_COMMAND[32];
-//     volatile uint16_t OLED_DATA[32];
-//     volatile uint16_t ADC[64];
-//     volatile uint16_t SRAM[128];
-// }AVR162_IO_MEMORY_REGS;
+#define SRAM 0x1800
+#define ADC 0x1400
+#define OLED_DATA 0x1200
+#define OLED_COMMAND 0x1000
 
 void xmem_init(void);
-void xmem_write(uint8_t data, uint16_t addr);
-uint8_t xmem_read(uint16_t addr);
+void xmem_write(uint8_t data, uint16_t addr, uint16_t base_address);
+uint8_t xmem_read(uint16_t addr, uint16_t base_address);
+
+#endif
