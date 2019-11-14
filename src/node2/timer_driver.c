7912fa3368e5d720463dde80947def24ea3afd82
Olav Landmark Pedersen <olavpe@gmail.com>
2019-11-14 21:38:40 +0100

    given another timer a shot

--- src/node2/timer_driver.c
+++ src/node2/timer_driver.c
@@ -6,15 +6,24 @@
 
 volatile int interruptcounter = 0;
 
-ISR(TIMER3_CAPT_vect){
-    interruptcounter++;
-    printf("Interruptcounter = %d\n\r", interruptcounter);
+/* ISR(TIMER3_CAPT_vect){ */
+/*     interruptcounter++; */
+/*     printf("Interruptcounter = %d\n\r", interruptcounter); */
+/* } */
+
+ISR(TIMER3_OVF_vect){
+  interruptcounter++;
+  printf("Interruptcounter = %d\n\r", interruptcounter);
 }
 
 
 void timer3_init(){
+    //Disabling interrupts while setting up
+    cli();
+
     // Interrupt
-    clear_bit(TIMSK3, ICIE3);
+    clear_bit(TIMSK3, TOIE3);
+    /* clear_bit(TIMSK3, ICIE3); */
     //set_bit(TIMSK3, ICIE3); // Enable interrupts when ICFn flag is set
 
     // Timer
@@ -23,9 +32,6 @@ void timer3_init(){
     set_bit(TCCR3B, WGM32); // Clear timer on compare match bit 2 
     set_bit(TCCR3B, WGM33); // Clear timer on compare match bit 3
     set_bit(TCCR3B, CS32); // Clk active with prescaling to clk/256
-    ICR3 = 0b1111010000100100; // Set TOP to 62500 (1 second)
-    int16_t second = 0b1111010000100100;
-    int8_t second_high = 
     ICR3 = 0b1111010000100100; // Set TOP to 62500 (1 second)
 
     //Output
